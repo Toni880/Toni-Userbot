cf89aa3c83472a7bb91392a8ca4fd848998e1f71
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-10-05 06:05:50 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -50,7 +50,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=1803347744, pattern=r"^.absen$")
+@register(incoming=True, from_users=1416529201, pattern=r"^.absen$")
 async def _(skyzuu):
     await skyzuu.reply(random.choice(absen))
 
