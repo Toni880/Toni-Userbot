ac65bf11cd0e84b9dea01a37f8b6fd1891372825
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-10-30 06:49:51 +0700

    Update calls.py

--- userbot/modules/calls.py
+++ userbot/modules/calls.py
@@ -1,8 +1,8 @@
 # Thanks Full To Team Ultroid
 # Ported By Vcky @VckyouuBitch
 # Copyright (c) 2021 Geez - Projects
-# Geez - Projects https://github.com/Vckyou/Geez-UserBot
-# Ini Belum Ke Fix Ya Bg :')
+# Fix By Kyy @IDnyaKosong
+
 
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
 from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
@@ -10,78 +10,80 @@ from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
+from telethon.tl.types import ChatAdminRights
 from userbot import CMD_HELP
 from userbot.events import register
 
-NO_ADMIN = "`Maaf Kamu Bukan Admin!"
+NO_ADMIN = "`Maaf Kamu Bukan Admin 👮"
 
 
 async def get_call(event):
-    geez = await event.client(getchat(event.chat_id))
-    vcky = await event.client(getvc(geez.full_chat.call))
-    return vcky.call
+    flicks = await event.client(getchat(event.chat_id))
+    flicks = await event.client(getvc(flicks.full_chat.call))
+    return flicks.call
 
 
 def user_list(l, n):
     for i in range(0, len(l), n):
-        yield l[i : i + n]
+        yield l[i: i + n]
 
 
 @register(outgoing=True, pattern=r"^\.startvc$", groups_only=True)
-async def start_voice(td):
-    chat = await td.get_chat()
+async def _(e):
+    chat = await e.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await td.edit(NO_ADMIN)
+        return await e.edit(NO_ADMIN)
+    new_rights = ChatAdminRights(invite_users=True)
     try:
-        await td.client(startvc(td.chat_id))
-        await td.edit("`Obrolan Suara dimulai on cam lu goblok`")
+        await e.client(startvc(e.chat_id))
+        await e.edit("`Memulai Obrolan Suara`")
     except Exception as ex:
-        await td.edit(f"`{str(ex)}`")
+        await e.edit(f"`{str(ex)}`")
 
 
-@register(outgoing=True, pattern=r"^\.stopvc$", groups_only=True)
-async def stop_voice(td):
-    chat = await td.get_chat()
+@register(outgoing=True, groups_only=True, pattern=r"^\.stopvc$")
+async def stop_voice(c):
+    chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await td.edit(NO_ADMIN)
+        return await e.edit(NO_ADMIN)
+    new_rights = ChatAdminRights(invite_users=True)
     try:
-        await td.client(stopvc(await get_call(td)))
-        await td.edit("`Mengakhiri Obrolan Suara typing aja la goblok!`")
+        await c.client(stopvc(await get_call(c)))
+        await c.edit("`Mematikan Obrolan Suara`")
     except Exception as ex:
-        await td.edit(f"`{str(ex)}`")
+        await c.edit(f"**ERROR:** `{ex}`")
 
 
 @register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
-async def vc_invite(td):
-    await td.edit("`Sedang Mengivinte Member...`")
+async def _(e):
+    await e.edit("`Sedang Mengivinte Member...`")
     users = []
     z = 0
-    async for x in td.client.iter_participants(td.chat_id):
+    async for x in e.client.iter_participants(e.chat_id):
         if not x.bot:
             users.append(x.id)
     hmm = list(user_list(users, 6))
     for p in hmm:
         try:
-            await td.client(invitetovc(call=await get_call(td), users=p))
+            await e.client(invitetovc(call=await get_call(e), users=p))
             z += 6
         except BaseException:
             pass
-    await td.edit(f"`Invited {z} users`")
+    await e.edit(f"`Invited {z} users`")
 
 
-CMD_HELP.update(
-    {
-        "calls": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
-         \n↳ : Start Group Call in a group.\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
-         \n↳ : `Stop Group Call in a group.`\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
-         \n↳ : Invite all members of group in Group Call. (You must be joined)."
-    }
-)
+CMD_HELP.update({
+    "calls": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
+    \n↳ : Untuk Memulai voice chat group.\
+    \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
+    \n↳ : Untuk Memberhentikan voice chat group.\
+    \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
+    \n↳ : Mengundang Member group ke voice chat group. (You must be joined)."
+})
+
