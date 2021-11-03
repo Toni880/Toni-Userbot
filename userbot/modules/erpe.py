399cb6a714d81a117996b82de58ec94d458472af
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-03 07:57:55 +0700

    Create erpe.py

---
+++ userbot/modules/erpe.py
@@ -0,0 +1,109 @@
+# import by skyzo
+# From Flicks Userbot
+# Do not delete credits ⚠️
+
+from time import sleep
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.adaerpe(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**ER ERRR ERRPE ASUU WOKWOK KANG PAKE PP ORANG KOREA**")
+    await typew.edit("`🏃.....ERPE KOMTOL.......🏃`")
+    await typew.edit("`🏃....................🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃.....`LARII`....🏃`")
+    await typew.edit("`🏃..............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃..Anjing...🏃`")
+    await typew.edit("`🏃.........🏃`")
+    await typew.edit("`🏃..Erpa..🏃`")
+    await typew.edit("`🏃...Erpe...🏃`")
+    await typew.edit("`🏃....Cringe..🏃`")
+    await typew.edit("`🏃....Tolol.....🏃`")
+    await typew.edit("`🏃............🏃`")
+    await typew.edit("`🏃..Tolong..🏃`")
+    await typew.edit("`🏃...........🏃`")
+    await typew.edit("`🏃............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃....Teyunkk...🏃`")
+    await typew.edit("`🏃.....Kejar.....🏃`")
+    await typew.edit("`🏃.....Sampai.....🏃`")
+    await typew.edit("`🏃.....Dapat.......🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃......Anjir........🏃`")
+    await typew.edit("`🏃....................🏃`")
+    await typew.edit("`🏃.....................🏃`")
+    await typew.edit("`🏃..Huh-Huh-Huh.......🏃`")
+    await typew.edit("`🏃...................🏃`")
+    await typew.edit("`🏃..................🏃`")
+    await typew.edit("`🏃.....Anak........🏃`")
+    await typew.edit("`🏃................🏃`")
+    await typew.edit("`🏃....Teyunkk....🏃`")
+    await typew.edit("`🏃..............🏃`")
+    await typew.edit("`🏃.............🏃`")
+    await typew.edit("`🏃..Jungkok Teyunk..🏃`")
+    await typew.edit("`🏃...........🏃`")
+    await typew.edit("`🏃..........🏃`")
+    await typew.edit("`🏃.........🏃`")
+    await typew.edit("__KOK MAKIN DEKET SI.__")
+    sleep(1)
+    await typew.edit("`🏃.......🏃`")
+    await typew.edit("`🏃...Taehyung..🏃`")
+    await typew.edit("`🏃.....🏃`")
+    await typew.edit("`🏃....🏃`")
+    await typew.edit("**Untung Ngga Kena Njir**")
+    sleep(1)
+    await typew.edit("__Cape Juga Asu Cringe Gitu__")
+    sleep(2)
+    await typew.edit("**Kasian Erpe Awokawokawok Makanya Jan Pake PP Orang KOREA**")
+
+
+@register(outgoing=True, pattern="^.erpe2(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await e.edit("HA ADA ERPE??")
+    await e.edit("HAHA")
+    await e.edit("HAHAHA")
+    await e.edit("HAHAHAHA")
+    await e.edit("HAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+    await e.edit("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA")
+
+
+CMD_HELP.update(
+    {
+        "erpe": "**Modules** - `erpe`\
+        \n\n Cmd : `.adaerpe`\
+        \nUsage : ngatain anak erpe\
+        \n\n Cmd : `.erpe2`\
+        \nUsage : ngetawain anak erpe\
+    "
+    }
+)
