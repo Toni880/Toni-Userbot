da81c9c87dfa3f60b8643873d8ae3801c4232914
Tonic990 <widodowarsito4@gmail.com>
2021-11-23 16:51:18 +0000

    Tonic-Userbot: auto fixes
    Signed-off-by: Tonic990 <widodowarsito4@gmail.com>

--- userbot/modules/paste.py
+++ userbot/modules/paste.py
@@ -6,7 +6,7 @@ from userbot.utils import edit_delete, edit_or_reply, man_cmd
 from userbot.utils.pastebin import PasteBin
 
 
-@man_cmd(pattern="paste(?: (-d|-n|-h|-k)|$)?(?: ([\s\S]+)|$)")
+@man_cmd(pattern="paste(?: (-d|-n|-h|-k)|$)?(?: ([\\s\\S]+)|$)")
 async def paste(pstl):
     """For .paste command, pastes the text directly to a pastebin."""
     service = pstl.pattern_match.group(1)
