820ce22a72b19ce2f4d89e0f39e0256d8df6e356
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-14 07:21:38 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -45,7 +45,7 @@ async def _(event):
         "INFO - Rose-Userbot: Logged in as 557667062",
         "INFO - Rose-Userbot: Successfully...",
         "919852+00:00 app[worker.1]: 919 - Rose-Userbot -",
-        "INFO - ⚡ Skyzu-Userbot ⚡ ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO - ✨ ᴛᴏɴɪᴄ ᴜsᴇʀʙᴏᴛ ✨ ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**",
     ]
 
