92e49e14f472f2b24850336afa3d520afdd0bb70
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-01-29 08:08:57 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from .format import parse_pre
 from .chrome import chrome, options
-from .decorator import rose_cmd, rose_handler
+from .decorator import toni_cmd, toni_handler
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
 from .events import checking, get_user_from_event
