63c7f9cc63bfbc0819be0f96fe67fc47e62c4871
Grey Project <95908917+grey423@users.noreply.github.com>
2022-02-02 12:12:52 -0800

    test

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -7,36 +7,33 @@ from telethon.tl.functions.contacts import UnblockRequest
 from userbot import BOT_USERNAME
 from userbot import CMD_HANDLER as cmd
 from userbot import bot
-from userbot.events import toni_cmd
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
+from userbot.utils import edit_or_reply, toni_cmd
 
 
-@bot.on(toni_cmd(outgoing=True, pattern=r"helpme"))
-async def yardim(event):
-    tgbotusername = BOT_USERNAME
-    if tgbotusername is not None:
+@toni_cmd(pattern="helpme")
+async def _(event):
+    if event.fwd_from:
+        return
+    if BOT_USERNAME is not None:
+        chat = "@Botfather"
         try:
-            results = await event.client.inline_query(
-                tgbotusername, "@PrimeSupportGroup"
-            )
+            results = await event.client.inline_query(BOT_USERNAME, "@PrimeSupportGroup")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
         except noinline:
-            event = await event.edit(
-                "🚧 **Tombol inline Kamu Tidak Aktif**.\n» __Sedang Menyalakannya, Harap Tunggu Sebentar__...",
+            xx = await edit_or_reply(
+                event,
+                "🚧 **Tombol inline Kamu Tidak Aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
             )
-            async with bot.conversation("@BotFather") as conv:
+            async with bot.conversation(chat) as conv:
                 try:
                     first = await conv.send_message("/setinline")
                     second = await conv.get_response()
                     third = await conv.send_message(BOT_USERNAME)
                     fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search...")
+                    fifth = await conv.send_message("Search")
                     sixth = await conv.get_response()
                     await bot.send_read_acknowledge(conv.chat_id)
                 except YouBlockedUserError:
@@ -45,17 +42,18 @@ async def yardim(event):
                     second = await conv.get_response()
                     third = await conv.send_message(BOT_USERNAME)
                     fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search...")
+                    fifth = await conv.send_message("Search")
                     sixth = await conv.get_response()
                     await bot.send_read_acknowledge(conv.chat_id)
-                await event.edit(
-                    f"✔️ **Berhasil Menyalakan Mode Inline.**\n\n» __Ketik {cmd}helpme Untuk Membuka inline Bantuan...__"
+                await xx.edit(
+                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helpme` **lagi untuk membuka menu bantuan.**"
                 )
             await bot.delete_messages(
                 conv.chat_id,
                 [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
             )
     else:
-        await event.edit(
+        await edit_or_reply(
+            event,
             "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
         )
