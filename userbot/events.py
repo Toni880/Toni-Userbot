2152c935522b15ec50e01c079c81a16dd0b14c1d
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-02-09 22:09:41 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,8 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
 """ Userbot module for managing events.
  One of the main components of the userbot. """
 
@@ -14,9 +12,7 @@ from asyncio import subprocess as asyncsub
 from pathlib import Path
 from time import gmtime, strftime
 from traceback import format_exc
-
 from telethon import events
-
 from userbot import CMD_HANDLER, LOGSPAMMER, CMD_HELP, bot
 
 
