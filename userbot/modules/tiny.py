945219899fb6ac0cf60ca8d9beb8fefc39375a30
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-02-14 22:45:26 +0700

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -4,11 +4,12 @@
 from PIL import Image
 import cv2
 import os
-from userbot.utils import toni_cmd
+from userbot.events import toni_cmd
+from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
 
 
-@toni_cmd(pattern="tiny(?: |$)(.*)")
+@bot.on(toni_cmd(outgoing=True, pattern="tiny(?: |$)(.*)"))
 async def _(event):
     reply = await event.get_reply_message()
     if not (reply and (reply.media)):
@@ -16,7 +17,7 @@ async def _(event):
         return
     xx = await event.edit("`Memproses Tiny....`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("resources/sky_blank.png")
+    im1 = Image.open("resources/extras/geez.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "geez.tgs")
         os.system("lottie_convert.py geez.tgs json.json")
@@ -82,5 +83,7 @@ async def _(event):
 
 
 CMD_HELP.update({
-    f"tiny": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}tiny`\
-    \nUsage : Untuk Memperkecil Sticker."})
+    "tiny":
+    f"✘ Plugin tiny :\
+\n\n  •  Perintah : `{cmd}tiny`\
+  \n  •  Fungsi : Untuk Memperkecil Sticker."})
