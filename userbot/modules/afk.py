93949d349fd24f3c8f8011d56887b2f3da39e225
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-10 11:16:14 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -293,7 +293,7 @@ async def afk_on_pm(sender):
 
 CMD_HELP.update(
     {
-        "afk": "`{cmd}afk` [Alasan]\
+        "afk": f"`{cmd}afk` [Alasan]\
 \nUsage: Lakukan ketika ingin OFF.\nSiapapun Yang Balas, Tag, Atau Chat Kamu \
 Mereka Akan Tau Alasan Kamu OFF.\n\nAFK Bisa Dilakukan Dan Dibatalkan Dimanapun.\
 "
