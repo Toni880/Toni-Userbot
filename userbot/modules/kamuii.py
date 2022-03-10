249db695d98c796e8823f11e92d004110eaab818
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-10 18:09:06 +0700

    Update kamuii.py

--- userbot/modules/kamuii.py
+++ userbot/modules/kamuii.py
@@ -1,19 +1,23 @@
 import os
-
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
-from userbot.events import register
+from userbot.utils import toni_cmd
+from userbot import (
+    TEMP_DOWNLOAD_DIRECTORY,
+    CMD_HANDLER as cmd,
+    CMD_HELP,
+    bot,
+)
 
 
-@register(outgoing=True, pattern=r"^.kamuii(:? |$)([1-8])?")
+@toni_cmd(pattern='kamuii(:? |$)([1-8])?')
 async def _(fry):
-    await fry.edit("`Prosess, mengaktifkan kekuatan Tonic...`")
+    await fry.edit("`Prosess, Mengaktifkan jurus tumor bolong...😜`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
     if not fry.reply_to_msg_id:
-        await fry.edit("`Mohon Balas Di Sticker blok`")
+        await fry.edit("`Mohon Balas Di Sticker`")
         return
     reply_message = await fry.get_reply_message()
     if not reply_message.media:
@@ -29,7 +33,9 @@ async def _(fry):
             msg = await conv.send_message(reply_message)
             if level:
                 m = f"/deepfry {level}"
-                msg_level = await conv.send_message(m, reply_to=msg.id)
+                msg_level = await conv.send_message(
+                    m,
+                    reply_to=msg.id)
                 r = await conv.get_response()
                 response = await conv.get_response()
             else:
@@ -37,34 +43,36 @@ async def _(fry):
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await fry.reply("`Mohon Unblock` @image_deepfrybot`...`")
+            await fry.reply("`Mohon Unblock` @image_deepfrybhuuhot`...`")
             return
         if response.text.startswith("Forward"):
             await fry.edit("`Mohon Matikan Setelan Forward Privasi...`")
         else:
             downloaded_file_name = await fry.client.download_media(
-                response.media, TEMP_DOWNLOAD_DIRECTORY
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
             )
             await fry.client.send_file(
                 fry.chat_id,
                 downloaded_file_name,
                 force_document=False,
-                reply_to=message_id_to_reply,
+                reply_to=message_id_to_reply
             )
             """ - cleanup chat after completed - """
             try:
                 msg_level
             except NameError:
-                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
             else:
                 await fry.client.delete_messages(
-                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
-                )
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
     await fry.delete()
     return os.remove(downloaded_file_name)
 
 
-@register(outgoing=True, pattern=r"^.df(:? |$)([1-8])?")
+@toni_cmd(pattern='df(:? |$)([1-8])?')
 async def _(fry):
     await fry.edit("`Sedang Dalam Proses......`")
     level = fry.pattern_match.group(2)
@@ -87,7 +95,9 @@ async def _(fry):
             msg = await conv.send_message(reply_message)
             if level:
                 m = f"/deepfry {level}"
-                msg_level = await conv.send_message(m, reply_to=msg.id)
+                msg_level = await conv.send_message(
+                    m,
+                    reply_to=msg.id)
                 r = await conv.get_response()
                 response = await conv.get_response()
             else:
@@ -101,30 +111,31 @@ async def _(fry):
             await fry.edit("`Mohon Matikan Setelan Privasi Forward...`")
         else:
             downloaded_file_name = await fry.client.download_media(
-                response.media, TEMP_DOWNLOAD_DIRECTORY
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
             )
             await fry.client.send_file(
                 fry.chat_id,
                 downloaded_file_name,
                 force_document=False,
-                reply_to=message_id_to_reply,
+                reply_to=message_id_to_reply
             )
             """ - cleanup chat after completed - """
             try:
                 msg_level
             except NameError:
-                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
             else:
                 await fry.client.delete_messages(
-                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
-                )
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
     await fry.delete()
     return os.remove(downloaded_file_name)
 
 
-CMD_HELP.update(
-    {
-        "kamuii": "`.kamuii` or `.kamuii` [level(1-8)]"
-        "\nUsage: untuk mengubah foto/sticker."
-    }
-)
+CMD_HELP.update({
+    "kamuii":
+    f"`.kamuii` or `{cmd}kamuii` [level(1-8)]"
+    "\nUsage: untuk mengubah foto/sticker."
+})
