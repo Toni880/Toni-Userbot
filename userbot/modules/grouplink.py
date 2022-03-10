b590b10c83dea11ff05b1d34ae8960c168f9771c
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-10 17:44:23 +0700

    Update grouplink.py

--- userbot/modules/grouplink.py
+++ userbot/modules/grouplink.py
@@ -1,10 +1,10 @@
 # port by KOALA 🐨 /@manusiarakitann
 
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot.utils import edit_or_reply, toni_cmd
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 
 
-@register(outgoing=True, pattern="^.gsend ?(.*)")
+@toni_cmd(pattern="gsend ?(.*)")
 async def remoteaccess(event):
 
     p = event.pattern_match.group(1)
@@ -21,21 +21,19 @@ async def remoteaccess(event):
     mssg = await event.get_reply_message()
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("`Pesan Di Di Teruskan Ke Grup Tujuan`")
+        await edit_or_reply(event, "`Pesan Di Di Teruskan Ke Grup Tujuan`")
     for i in m[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("Pesan Di Di Teruskan Ke Grup Tujuan`")
+        await edit_or_reply(event, "Pesan Di Di Teruskan Ke Grup Tujuan`")
     except BaseException:
-        await event.edit("** Gagal Mengirim Pesan, Emang Lu Join Grup Nya Goblok ? **")
-
+        await edit_or_reply(event, "** Gagal Mengirim Pesan, Emang Lu Join Grup Nya Goblok ? **")
 
 CMD_HELP.update(
     {
-        "grouplink": ".gsend\
+        "grouplink": f"{cmd}gsend\
     \nMengirim Pesan Jarak Jauh Ke Grup Lain .gsend <link grup> <pesan>."
-    }
-)
+    })
