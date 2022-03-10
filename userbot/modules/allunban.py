dfcc1b92afa5361c06eb2bfef837960966d79126
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-10 08:37:15 +0700

    Custom Handler

--- userbot/modules/allunban.py
+++ userbot/modules/allunban.py
@@ -2,13 +2,15 @@
 # Copyright © 2021 Geez-Projects
 from telethon.tl.types import ChannelParticipantsKicked
 
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot import (
+    CMD_HELP,
+    CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, toni_cmd
 
 
-@register(outgoing=True, pattern=r"^\.allunban(?: |$)(.*)", groups_only=True)
+@toni_cmd(pattern="allunban(?: |$)(.*)")
 async def _(event):
-    await event.edit("`Sedang Mencari List Banning.`")
+    xx = await edit_or_reply(event, "`Sedang Mencari List Banning.`")
     p = 0
     (await event.get_chat()).title
     async for i in event.client.iter_participants(
@@ -21,12 +23,12 @@ async def _(event):
             p += 1
         except BaseException:
             pass
-    await event.edit("`Sukses Menghapus List Banning`")
+    await xx.edit("`Sukses Menghapus List Banning`")
 
 
 CMD_HELP.update(
     {
-        "allunban": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.allunban`\
+        "allunban": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}allunban`\
     \n↳ : Membatalkan semua Ban Di Anggota Grup."
     }
 )
