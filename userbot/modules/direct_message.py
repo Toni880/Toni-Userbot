58bc82a7d20dd2de8db6adcd910581ca5abf047e
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-10 15:53:09 +0700

    Update direct_message.py

--- userbot/modules/direct_message.py
+++ userbot/modules/direct_message.py
@@ -1,11 +1,8 @@
-from userbot import CMD_HELP
-from userbot.events import register
-
-# Ported By @VckyouuBitch From GeezProject
-# Devoted To GeezProject
+from userbot.utils import edit_delete, edit_or_reply, toni_cmd
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 
 
-@register(outgoing=True, pattern=r"^\.(?:dm)\s?(.*)?")
+@toni_cmd(pattern="(?:dm)\\s?(.*)?")
 async def remoteaccess(event):
 
     p = event.pattern_match.group(1)
@@ -22,21 +19,19 @@ async def remoteaccess(event):
     mssg = await event.get_reply_message()
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("`Success Mengirim Pesan Anda.`")
+        await edit_or_reply(event, "`Success Mengirim Pesan Anda.`")
     for i in m[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("`Success Mengirim Pesan Anda.`")
+        await edit_or_reply(event, "`Success Mengirim Pesan Anda.`")
     except BaseException:
-        await event.edit("**Terjadi Error. Gagal Mengirim Pesan.**")
-
+        await edit_delete(event, "**Terjadi Error. Gagal Mengirim Pesan.**")
 
 CMD_HELP.update(
     {
-        "message": "`.dm`\
+        "message": f"`{cmd}dm`\
     \nMengirim Pesan Dengan Jarak Jauh Dengan .dm <username> <pesan>."
-    }
-)
+    })
