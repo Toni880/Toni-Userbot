f49a6b532faaed6c9baed7fd59077c42e8668729
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-11 09:36:37 +0700

    Update toxic2.py

--- userbot/modules/toxic2.py
+++ userbot/modules/toxic2.py
@@ -1,99 +1,150 @@
 from time import sleep
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, toni_cmd
 
-from userbot import CMD_HELP
-from userbot.events import register
 
-
-@register(outgoing=True, pattern="^.ngentot(?: |$)(.*)")
+@toni_cmd(pattern='ngentot(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("**WOYY NGENTOD!!**")
+    xx = await edit_or_reply(typew, "**WOYY NGENTOD!!**")
     sleep(1)
-    await typew.edit("**JANGAN SOK JAGOAN DAH LU**")
+    await xx.edit("**JANGAN SOK JAGOAN DAH LU**")
     sleep(1)
-    await typew.edit("**MUKA MASIH KAYA KONTOL AJA**")
+    await xx.edit("**MUKA MASIH KAYA KONTOL AJA**")
     sleep(1)
-    await typew.edit("**BANGGA LU HAHAHAHA**")
+    await xx.edit("**BANGGA LU HAHAHAHA**")
     sleep(1)
-    await typew.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
+    await xx.edit("**COBA DEH NGACA MUKA LU KAN HINA BANGET**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await xx.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**MAKANYA GANTENG KONTOL**")
+    await xx.edit("**MAKANYA GANTENG KONTOL**")
     sleep(1)
-    await typew.edit("**BIAR MUKALU GAK DIHINA TERUS**")
+    await xx.edit("**BIAR MUKALU GAK DIHINA TERUS**")
     sleep(1)
-    await typew.edit("**SAMA ORANG LAIN**")
+    await xx.edit("**SAMA ORANG LAIN**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
-
-
+    await xx.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern="^.goblok(?: |$)(.*)")
+@toni_cmd(pattern='goblok(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("**WOYY GOBLOK!!**")
+    xx = await edit_or_reply(typew, "**WOYY GOBLOK!!**")
     sleep(1)
-    await typew.edit("**KO LU GOBLOK BANGET SIH**")
+    await xx.edit("**KO LU GOBLOK BANGET SIH**")
     sleep(1)
-    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    await xx.edit("**OTAK LU TUH KAYA KONTOL**")
     sleep(1)
-    await typew.edit("**YANG LEMBEK KETIKA LEMAH**")
+    await xx.edit("**YANG LEMBEK KETIKA LEMAH**")
     sleep(1)
-    await typew.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
+    await xx.edit("**DAN KERAS KETIKA LU SANGE GOBLOK**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await xx.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    await xx.edit("**MAKANYA JANGAN SANGEAN MULU**")
     sleep(1)
-    await typew.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
+    await xx.edit("**MUKA LU AJA KAYA ASPAL JALANAN**")
     sleep(1)
-    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    await xx.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
-
-
+    await xx.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern="^.ngatain(?: |$)(.*)")
+@toni_cmd(pattern='ngatain(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("**BABI!!**")
+    xx = await edit_or_reply(typew, "**BABI!!**")
     sleep(1)
-    await typew.edit("**MUKA LU KAYA BABI**")
+    await xx.edit("**MUKA LU KAYA BABI**")
     sleep(1)
-    await typew.edit("**OTAK LU TUH KAYA KONTOL**")
+    await xx.edit("**OTAK LU TUH KAYA KONTOL**")
     sleep(1)
-    await typew.edit("**MUKA LU HINA BANGET**")
+    await xx.edit("**MUKA LU HINA BANGET**")
     sleep(1)
-    await typew.edit("**OTAK LU KAYA BATU**")
+    await xx.edit("**OTAK LU KAYA BATU**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
+    await xx.edit("**HAHAHAHA**")
     sleep(1)
-    await typew.edit("**MAKANYA JANGAN SANGEAN MULU**")
+    await xx.edit("**MAKANYA JANGAN SANGEAN MULU**")
     sleep(1)
-    await typew.edit("**KONTOL LU AJA MASIH BENGKOK**")
+    await xx.edit("**KONTOL LU AJA MASIH BENGKOK**")
     sleep(1)
-    await typew.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
+    await xx.edit("**EHHH SANGE NYA MAU DAPAT YANG CANTIK**")
     sleep(1)
-    await typew.edit("**HAHAHAHA**")
-
-
+    await xx.edit("**HAHAHAHA**")
 # Create by myself @localheart
 
-CMD_HELP.update(
-    {
-        "toxic2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngentot`\
-    \n↳ : Lu Coba Sendiri Aja."
-        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.goblok`\
-    \n↳ : Lu Coba Sendiri Aja."
-        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ngatain`\
-    \n↳ : Lu Coba Sendiri Aja."
-    }
-)
+
+@toni_cmd(pattern='yatim(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    xx = await edit_or_reply(typew, "`Hai Anak Kontol 🙈, Jangan Lupa Makan Yaa`")
+    sleep(1)
+    await xx.edit("`Jangan Bilang Lu Ga Dikasih Makan Sama Ortu 😁`")
+    sleep(1)
+    await xx.edit("`APA PERLU GUA SANTUNIN ?? 🙈🙈 xixixi`")
+    sleep(1)
+    await xx.edit("`OH IYAA LUPAAA, LU KAN BEBAN KELUARGA 🤣`")
+    sleep(1)
+    await xx.edit("`MANA MUNGKIN ORTU LU PEDULII xixixi 🙈`")
+    sleep(1)
+    await xx.edit("`KETAWA DULU BOLEH KALI YAA 😁`")
+    sleep(1)
+    await xx.edit("`HAHAHAHAHAHAHA`")
+    sleep(1)
+    await xx.edit("`KASIAN ORTUNYAA GAPEDULIII 🙈🤣`")
+    sleep(1)
+    await xx.edit("`MAAF YA, CANDAA BEBANNNN xixixi 🙈`")
+    sleep(1)
+    await xx.edit("`Tapi Bo'ong Hiyahiyahiya`")
+# Create by myself @localheart
+
+
+@toni_cmd(pattern="kont(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    xx = await edit_or_reply(typew, "**KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**LU ANAK KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**DI BIKIN DARI KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**MUKALU PERSIS KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**DASAR ANAK NGONTOLLLL**")
+    sleep(1.5)
+    await xx.edit("**NOLEP KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**NGERUSUH KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**BENER BENER KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**PADAHAL LO GAPUNYA KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**MENDING LO OPERASI KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**BIAR LO PUNYA KONTOLL**")
+    sleep(1.5)
+    await xx.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+
+
+CMD_HELP.update({
+    "toxic2": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ngentot`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}goblok`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}ngatain`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}kont`\
+    \n↳ : Cobain aja\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}yatim`\
+    \n↳ : Cobain aja."
+})
