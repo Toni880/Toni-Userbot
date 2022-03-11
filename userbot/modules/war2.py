69a5e0eec1ba256a973cf9c40969b9efb6365c09
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-11 10:41:36 +0700

    Update war2.py

--- userbot/modules/war2.py
+++ userbot/modules/war2.py
@@ -1,176 +1,130 @@
 # ReCode by @skyzu
 # FROM skyzu-userbot <https://github.com/Skyzu/skyzu-userbot>
-# KONTOLLLL
+# KONTOLLLLLLL
 
-from time import sleep
+from platform import uname
+from userbot import (
+    ALIVE_NAME,
+    CMD_HELP,
+    CMD_HANDLER as cmd,
+)
+from userbot.utils import edit_or_reply, toni_cmd
 
-from userbot import CMD_HELP
-from userbot.events import register
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
 
 
-@register(outgoing=True, pattern=r"^\.sok(?: |$)(.*)")
+@toni_cmd(pattern="emak(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1.5)
-    await typew.edit("**WOII**")
-    sleep(1.5)
-    await typew.edit("**KONTOL**")
-    sleep(1.5)
-    await typew.edit("**KALO MENTAL MASIH PATUNGAN**")
-    sleep(1.5)
-    await typew.edit("**GAUSAH SOK KERAS DEH**")
-    sleep(1.5)
-    await typew.edit("**GA KEREN LO BEGITU NGENTOT**")
+    await edit_or_reply(typew,
+                        "**PUNYA EMAK JANGAN JELEK GUE TAU EMAK LO UDAH MUKA JELEK ITEM BAT ITEM KEK OLI MOTOR BELOM DI GANTI SETAHUN HAHA TERUS BADANNYA GENDUT BAT GENDUT TETENYA KONDOR LAGI PANTESAN AJA KAGA LAKU HAHAHA NIH GUA KASIH TAU YE SAMA LO BOKAP LO AJE KERJAANNYA MAEN JUDI MULU BORO BORO MENANG BOKAP LO AJE MAEN JUDI KALAH MULU TOLOL HAHA UDAH NGUTANG SANA SINI AMPE DI KEJAR DEB COLLECTOR ITU BOKAP LO LAGI DI BURU OLEH DEP COLECTOR MANGKANYA KALO UDAH TAU MISKIN KERJAANNYA JANGAN MAEN JUDI MULU TOLOL SAMA NGUTANG CARI KERJA YANG HALAL SONO HAHA**"
+                        )
 
 
-@register(outgoing=True, pattern="^.wah(?: |$)(.*)")
+@toni_cmd(pattern="ehh(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wahh, War nya keren bang`")
-    sleep(2)
-    await typew.edit("`Tapi, Yang gua liat, kok Kaya lawakan`")
-    sleep(2)
-    await typew.edit("`Oh iya, Kan lo badut 🤡`")
-    sleep(2)
-    await typew.edit("`Kosa kata pas ngelawak, Jangan di pake war bang`")
-    sleep(2)
-    await typew.edit("`Kesannya lo ngasih kita hiburan.`")
-    sleep(2)
-    await typew.edit(
-        "`Kasian badut🤡, Ga di hargain pengunjung, Eh lampiaskan nya ke Tele, Wkwkwkwk`"
-    )
-    sleep(3)
-    await typew.edit("`Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak`")
+    await edit_or_reply(typew,
+                        "**EHH TOLOL PEKERJAAN KAKEK LO DULU ITU JADI BABU BABU KOMPENI YANG DI SURUH BIKININ KOPI TERUS MOTONG RUMPUT DI HALAMAN RUMAH YAELAH KASIAN BAT KASIAN KAKEK LO ITU UDAH KONTET KURUS KERING KEREMPENG LAGI KAKEK LO DULU MATINYA KE ABISAN TENAGA PAS NYABUTIN RUMPUT TOLOL HAHA NENEK LO JUGA JADI LACUR KOMPENI NENEK LO AJE JADI LACUR KOMPENI CUMA DI BAYAR PAKE SINGKONG REBUS DOANG SATU BIJI TERUS NENEK LO ITU MATINYA PAS DI GANGBANG SAMA TENTARA KOMPENI MEMEK NENEK LO ITU DI SODOK SODOK MAKE SENAPAN NOH SAMPE MEMEKNYA LOBEH LEBAR BAT LEBAR KEK JALAN RAYA HAHA.BADUR BADUT IYE GUE TAU LO MAIN TELE ITU DI JADIIN BADUT ALIANSI KAN HAHAHA MANGKANYA BANG KALO JADI ORANG PAS DI SENGGOL LAWAN JANGAN DIEM BAE KEK BATU HAHA DI JADIIN BADUT KAN LO DI LEDEK LEDEKIN SAMA SEMUA ALIANSI,GUE TAU TUJUAN LO MAEN TELE ITU UNTUK MENCARI MEMEK MEMEK SEGAR KAN HAHA KETAUAN ORANG ORANG KEK LO OTAK SANGEAN YANG HAUS AKAN MEMEK DAN TOKET SAKING GK MAU MODAL DI RL BUAT NYEWA LACUR JADI LO MEMILIH BIAT MAIN TELE BORO BORO DAPET LAH LU KAGA DAPET SAMA SEKALI MANGKANYA GANTENG KONTOL LO UDAH JELEK PENGENNYA YANG BAGUS BAGUS NYADAR DIRI LO ITU UDAH JELEK TERUS MISKIN LAGI**")
 
 
-@register(outgoing=True, pattern=r"^\.kont(?: |$)(.*)")
+@toni_cmd(pattern="mas(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("**KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**LU ANAK KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**DI BIKIN DARI KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**MUKALU PERSIS KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**DASAR ANAK NGONTOLLLL**")
-    sleep(1.5)
-    await typew.edit("**NOLEP KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**NGERUSUH KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**BENER BENER KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**PADAHAL LO GAPUNYA KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**MENDING LO OPERASI KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**BIAR LO PUNYA KONTOLL**")
-    sleep(1.5)
-    await typew.edit("**KASIAN CACAD GAPUNYA KONTOLL**")
+    await edit_or_reply(typew,
+                        "**MAS KALO UDA NGANTUK BOBO AJA JANGAN MAKSAIN TERUS BUAT LAWAN GUA TAKUTNYA LO BESOK KENA ANGIN DUDUK DOANG TERUS MATI DAH KAN KASIAN UDAH JADI BEBAN KELUARGA MALAH NAMBAHIN BEBAN TOLOL. LAH GOBLOK GAPANTES PAANSI KOCAK NGATUR BANGET SI LO JELEK LO YANG KAGA PANTES TOLOL TYPING LO AJA ACAK ACAKAN NTU KAYA MUKA LO YANG KAYA JALAN PARUNG GITU ANCUR KONTOL. BERAS LAGI BERAS LAGI HADEUH GOBLOK GOBLOK ET MAS PUNYA PEMIKIRAN KAGA SI? ET IYA LUPA OTAK BE ORA ADA APALAGI PEMIKIRAN YA WKWK GOBLOK. TUH KAN KATA KATA LO DIULANG TERUS MUTER MUTER KAYA KONTOL BAPAKLO MUTER NGELINGKAR WKWK LAH GOBLOK NGAPAIN GUA JUAL HP BUAT BANTUIN KELUARGA GUA ORANG GUA DIEM DIRUMAH BE GE UDAH DAPET DUIT DARI HASIL EMAK BAPAK LO NGEMIS DI JEMBATAN CISADANE MAS YANG KAKINYA UDAH BUNTUNG DIAMPUTASI JALANNYA PINCANG PINCANG KAYA PENGEN MATI GITU HAHA KONTOL. TUH EPEP TERUS LO MA AH MENDING LO BOBO SONO KALO KAGA LO NANGIS DIPOJOKAN SEBARI MAININ TITIT ABIS ITU LO NGADU KE BAPAK LO KALO LO DIHINA HINA TERUS SM GUA SAMPE KENA MENTAL TERUS STRESS DAH LO KAYA SEKARANG WKWK NGENTOT**"
+                        )
 
 
-@register(outgoing=True, pattern=r"^\.an(?: |$)(.*)")
+@toni_cmd(pattern="dih(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(
-        "**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**"
-    )
+    await edit_or_reply(typew,
+                        "**DIHHH NAJISS ANAK HARAM LO GOBLOK, JANGAN BELAGU DIMARI KAGA KEREN LU KEK BGITU KONTOL!**"
+                        )
 
 
-@register(outgoing=True, pattern=r"^\.bk(?: |$)(.*)")
+@toni_cmd(pattern="gcs(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(
-        "**BUAT LO KONTOL NIH KALO UDAH HINA GAUSAH SOK SOK NGEHINA HINA GUA KONTOL, GUA TERLALU SUCI BUAT LU YANG HINA ITU ADUHHH. SINI GUA LUDAHIN DLU LU BIAR DIRI LU SUCI KARENA LU TAU LUDAH GUA ITU MULIA SEKALI**"
-    )
+    await edit_or_reply(typew, "**GC SAMPAH KAYA GINI, BUBARIN AJA GOBLOK!!**"
+                        )
 
 
-@register(outgoing=True, pattern=r"^\.gj(?: |$)(.*)")
+@toni_cmd(pattern="skb(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(
-        "**YA AMPUN LU NGOMONG APA? GA NYAMBUNG KONTOL KAYA KEHIDUPAN LU MAKANYA ORG ORG KAYA LU GABAKALN MAJU HIDUPNYA APA LAGI ORG ORG BAWAHAN KAYA LU.**"
-    )
+    await edit_or_reply(typew, "**EMANG KITA KENAL? KAGA GOBLOK SOKAB BANGET LU GOBLOK**"
+                        )
 
 
-@register(outgoing=True, pattern=r"^\.gh(?: |$)(.*)")
+@toni_cmd(pattern="an(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(
-        "**DUH GINI NIH BOCAH YG LAHIR DI GUBUK BAMBU REOT + GAPUNYA HARGA DIRI, PADAHAL MAH DARI KECIL DIAJARIN SM EMAKNYA GABOLEH SONGONG SM MAJIKAN MASIH AJA SONGONG, MENDING LO URUSIN DULU GOBLOK KELUARGA LO YG PENYAKITAN ITU, MANA BAPA LO KAKINYA BOROK BEGITU AJG BERNANAH BAU AMIS IDIH GELI BET GELI GUA LIATNYA, NAH SEKALIAN TUH URUSIN JUGA ADE LO TUH, KALO BUKAN KARENA GUA MAH ADE LO UDAH MENINGGAL KENA TUMOR TOLOL MAKANYA LO KUDU SUJUD DEPAN GUA YAKAN,EMAK LO JUGA TUH JAGAIN UDAH BISU BEGITU YAKAN TAKUTNYA JATOH GABISA TREAK, MAKANYA NIH YA JANGAN KEBANYAKAN KONSUMSI SASA MICIN GOBLOK LIAT KAN EFEKNYA LO JADI KEK BOCAH AYAN BEGITU, SAMPE² LO BERANI GITU YAKAN NYENGGET JEMURAN ORANG SAMPE LO DIPUKULIN TRUS DI INJEK² SAMA WARGA SEKAMPUNGAN, GINI YA GUA KASIH TAU NIH SAMA LO NIH KALO UDA MISKIN KAGA USAH BELAGU SEGALA TOLOL, MIKIR LO MAKAN AJA SUSAH SAMPE NGEMIS² DI KOMPLEK PERUMAHAN GUA SAMPE DI USIR SAMA SATPAM KOMPLEK GUA, BERAS AJA LO BOLEH DIBAGI SAMA EMAK GUA YAKAN LAUK PAUK IKAN, AYAM, DAGING SEGALA RUPA AJA LO BOLEH NYOLONG DARI PASAR BOCAH KAYA LO MAH GIZINYA KURANG DONGO SABAN HARI MAKAN INDOMI 1 PAKE TELOR DOANG ITU JUGA JOINAN SM KELUARGA LU, KARENA APA?, YA KARENA LO MISKIN GA MAMPU BELI MAKANAN YG BERGIZI, DIKASIH KUAH SAYUR KANGKUNG JUGA MAO TOLOL ITU JUGA UDAH BERSYUKUR BISA MAKAN MAKANAN SELAEN MI INSTAN YAKAN SECARA LO GABISA GITU KEK GUA YAKAN MAKAN APA YG GUA MAO LAH ELO MAKAN MAKANAN TONG SAMPAH JUGA UDAH ALHAMDULILLAH BANGET AJG**"
-    )
+    await edit_or_reply(typew,
+                        "**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**"
+                        )
 
 
-@register(outgoing=True, pattern=r"^\.lol(?: |$)(.*)")
+@toni_cmd(pattern="bk(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(
-        "**TALAL TOLOL TALAL TOLOL KEK ORG DONGO, NIH YA DENGERIN MAKANYA BESOK² JANGAN NGELEM MULU OTAK LO, JADI LEMBEK DAH TUH PALA, MANA SABAN HARI MAKE SAMPO SACHET YG SEREBUAN GIMANA GA KETOMBEAN TU PALA, YA JELAS LAH BEDA SAMA GUA YA GUA SI PAKE SAMPO BOTOLAN GITU YAKAN, MANA SABUN BETAK DI RUMAH ORANG BEKAS COLI JUGA DIPAKE SAMA KELUARGA LO BUAT MANDI YA KARENA APA?, KARNA KELUARGA LO TUH HINA TOLOL BAHKAN LEBIH HINA DARIPADA HEWAN, LO KAGA PANTES MAEN TELE SUMPAH MENDING LO SEKOLAH DULU DAH YG BENER DI BIAYAIN AMA PEMERINTAH AJA GATAU DIRI KONTOL, LO PIKIR BAPA EMAK LO MAMPU GITU BIAYAIN LO SEKOLAH?, KAGA GOBLOK GUA MAH TAU BET TAU LO TU CUMA BABU DI TONGKRONGAN KAN, PALING² JUGA YA LO NONGKRONG BAWA GOCENG YA MANA ADA YG MAO TEMENAN AM ORG SUSAH JELEK HINA DEKIL KAYA LO TOLOL, APALAGI CEWE, MANA ADA CEWE YG MAO AM ORG KAYA LO, PALINGAN JUGA DIAJAK JALANNYA KE RAWA² YAKAN MANA MAO NGEWE TAPI GAMAO MODAL YA JADI NGEWENYA DI SEMAK² RAWA DAH TUH MANA DIGIGITIN SEMUT RANGRANG SAMPE PADA BURIK TUH KAKI CEWE LO, BUKANNYA MIKIR GITU YAKAN MALAH PLANGA PLONGO TAWA TAWA KEK BOCAH BLOON**"
-    )
+    await edit_or_reply(typew,
+                        "**BUAT LO KONTOL NIH KALO UDAH HINA GAUSAH SOK SOK NGEHINA HINA GUA KONTOL, GUA TERLALU SUCI BUAT LU YANG HINA ITU ADUHHH. SINI GUA LUDAHIN DLU LU BIAR DIRI LU SUCI KARENA LU TAU LUDAH GUA ITU MULIA SEKALI**"
+                        )
 
 
-@register(outgoing=True, pattern="^.title(?: |$)(.*)")
+@toni_cmd(pattern="gj(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**OI ANAK TITLE**")
-    sleep(2)
-    await typew.edit("**OOO INI YANG SOK JADI PAHLAWAN DI TELEGRAM?**")
-    sleep(3)
-    await typew.edit("**TITLE KEMANA MANA SAMPE MENUHIN NAMA**")
-    sleep(2)
-    await typew.edit("**ADA YANG SAMPE 18+ LAH SEGALA MACEM**")
-    sleep(2)
-    await typew.edit("**LO KIRA KEREN KEK GITU?**")
-    sleep(2)
-    await typew.edit("**KERJAAN CUMA NGURUSIN GRUP DI TELEGRAM SAMA NGAJAK ORANG WAR**")
-    sleep(4)
-    await typew.edit("**YAELAH BRO MENTAL LO CUMA DI SOSMED APA GIMANE?**")
-    sleep(2)
-    await typew.edit(
-        "**PERASAAN DULU TELEGRAM GAADA DEH BOCAH BOCAH SOK JAGO KEK GINI**"
-    )
-    sleep(2)
-    await typew.edit("**GILIRAN TITLE NYA DI EJEK NGADU KE OWNER NYA**")
-    sleep(4)
-    await typew.edit("**TRUS NGAJAK WAR**")
-    sleep(2)
-    await typew.edit("**BUSET DAH BANG**")
-    sleep(2)
-    await typew.edit("**UDAH SEJAGO APESI SAMPE GC DIBELA BELA**")
-    sleep(3)
-    await typew.edit("**ORANG TUA LO NOH ADA YANG NAGIH UTANG UDA LO BELA BELOM?**")
-    sleep(4)
-    await typew.edit("**RELA NGUTANG DEMI NGIDUPIN LU**")
-    sleep(2)
-    await typew.edit("**EH ANAKNYA MALAH NGEBELAIN GC GAJELAS HAHAHA**")
-    sleep(3)
-    await typew.edit("**MANA VIRTUAL LAGI, SOK JAGO LAGI DUH**")
-    sleep(3)
-    await typew.edit("**SEMOGA CEPET SADAR YA HAHAHAHA**")
+    await edit_or_reply(typew,
+                        "**YA AMPUN LU NGOMONG APA? GA NYAMBUNG KONTOL KAYA KEHIDUPAN LU MAKANYA ORG ORG KAYA LU GABAKALN MAJU HIDUPNYA APA LAGI ORG ORG BAWAHAN KAYA LU.**"
+                        )
+
+
+@toni_cmd(pattern="gh(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew,
+                        "**DUH GINI NIH BOCAH YG LAHIR DI GUBUK BAMBU REOT + GAPUNYA HARGA DIRI, PADAHAL MAH DARI KECIL DIAJARIN SM EMAKNYA GABOLEH SONGONG SM MAJIKAN MASIH AJA SONGONG, MENDING LO URUSIN DULU GOBLOK KELUARGA LO YG PENYAKITAN ITU, MANA BAPA LO KAKINYA BOROK BEGITU AJG BERNANAH BAU AMIS IDIH GELI BET GELI GUA LIATNYA, NAH SEKALIAN TUH URUSIN JUGA ADE LO TUH, KALO BUKAN KARENA GUA MAH ADE LO UDAH MENINGGAL KENA TUMOR TOLOL MAKANYA LO KUDU SUJUD DEPAN GUA YAKAN,EMAK LO JUGA TUH JAGAIN UDAH BISU BEGITU YAKAN TAKUTNYA JATOH GABISA TREAK, MAKANYA NIH YA JANGAN KEBANYAKAN KONSUMSI SASA MICIN GOBLOK LIAT KAN EFEKNYA LO JADI KEK BOCAH AYAN BEGITU, SAMPE² LO BERANI GITU YAKAN NYENGGET JEMURAN ORANG SAMPE LO DIPUKULIN TRUS DI INJEK² SAMA WARGA SEKAMPUNGAN, GINI YA GUA KASIH TAU NIH SAMA LO NIH KALO UDA MISKIN KAGA USAH BELAGU SEGALA TOLOL, MIKIR LO MAKAN AJA SUSAH SAMPE NGEMIS² DI KOMPLEK PERUMAHAN GUA SAMPE DI USIR SAMA SATPAM KOMPLEK GUA, BERAS AJA LO BOLEH DIBAGI SAMA EMAK GUA YAKAN LAUK PAUK IKAN, AYAM, DAGING SEGALA RUPA AJA LO BOLEH NYOLONG DARI PASAR BOCAH KAYA LO MAH GIZINYA KURANG DONGO SABAN HARI MAKAN INDOMI 1 PAKE TELOR DOANG ITU JUGA JOINAN SM KELUARGA LU, KARENA APA?, YA KARENA LO MISKIN GA MAMPU BELI MAKANAN YG BERGIZI, DIKASIH KUAH SAYUR KANGKUNG JUGA MAO TOLOL ITU JUGA UDAH BERSYUKUR BISA MAKAN MAKANAN SELAEN MI INSTAN YAKAN SECARA LO GABISA GITU KEK GUA YAKAN MAKAN APA YG GUA MAO LAH ELO MAKAN MAKANAN TONG SAMPAH JUGA UDAH ALHAMDULILLAH BANGET AJG**"
+                        )
+
+
+@toni_cmd(pattern="lol(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew,
+                        "**TALAL TOLOL TALAL TOLOL KEK ORG DONGO, NIH YA DENGERIN MAKANYA BESOK² JANGAN NGELEM MULU OTAK LO, JADI LEMBEK DAH TUH PALA, MANA SABAN HARI MAKE SAMPO SACHET YG SEREBUAN GIMANA GA KETOMBEAN TU PALA, YA JELAS LAH BEDA SAMA GUA YA GUA SI PAKE SAMPO BOTOLAN GITU YAKAN, MANA SABUN BETAK DI RUMAH ORANG BEKAS COLI JUGA DIPAKE SAMA KELUARGA LO BUAT MANDI YA KARENA APA?, KARNA KELUARGA LO TUH HINA TOLOL BAHKAN LEBIH HINA DARIPADA HEWAN, LO KAGA PANTES MAEN TELE SUMPAH MENDING LO SEKOLAH DULU DAH YG BENER DI BIAYAIN AMA PEMERINTAH AJA GATAU DIRI KONTOL, LO PIKIR BAPA EMAK LO MAMPU GITU BIAYAIN LO SEKOLAH?, KAGA GOBLOK GUA MAH TAU BET TAU LO TU CUMA BABU DI TONGKRONGAN KAN, PALING² JUGA YA LO NONGKRONG BAWA GOCENG YA MANA ADA YG MAO TEMENAN AM ORG SUSAH JELEK HINA DEKIL KAYA LO TOLOL, APALAGI CEWE, MANA ADA CEWE YG MAO AM ORG KAYA LO, PALINGAN JUGA DIAJAK JALANNYA KE RAWA² YAKAN MANA MAO NGEWE TAPI GAMAO MODAL YA JADI NGEWENYA DI SEMAK² RAWA DAH TUH MANA DIGIGITIN SEMUT RANGRANG SAMPE PADA BURIK TUH KAKI CEWE LO, BUKANNYA MIKIR GITU YAKAN MALAH PLANGA PLONGO TAWA TAWA KEK BOCAH BLOON**"
+                        )
 
 
 CMD_HELP.update(
     {
-        "war2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .sok\
-         \n↳ : ngatain orang yang sok keras\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .bk\
-         \n↳ : ngatain bocah hina\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gh\
-         \n↳ : lihat sendiri\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .wah\
-         \n↳ : lihat sendiri\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .kont\
-         \n↳ : ngatain anak kontol\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gj\
-         \n↳ : ngatain bocah gajelas\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .lol\
-         \n↳ : ngatain bocah tolol\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .title\
-         \n↳ : ngatain bocah gila title\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .an\
-         \n↳ : ngatain anak pungut"
+        "war2": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}ehh\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}emak\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}mas\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}dih\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}gcs\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}skb\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}an\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}bk\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}gj\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}gh\
+        \n↳ : lihat sendiri\
+        \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: {cmd}lol\
+        \n↳ : lihat sendiri\
+        \n↳ **COBAIN AJA SENDIRI SEMUA!**.\
+    "
     }
 )
