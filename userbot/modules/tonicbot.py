4debfe5dcee5021986026bd0d1f2cceb6521ef93
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-11 09:08:55 +0700

    Update tonicbot.py

--- userbot/modules/tonicbot.py
+++ userbot/modules/tonicbot.py
@@ -1,85 +1,60 @@
 from time import sleep
 
-from userbot import CMD_HELP
-from userbot.events import register
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import toni_cmd, edit_or_reply
 
 
-@register(outgoing=True, pattern="^.sadboy(?: |$)(.*)")
+@toni_cmd(pattern="sadboy(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(2)
-    await typew.edit("`Pertama-tama kamu cantik`")
+    sad = await edit_or_reply(typew, "`Pertama-tama kamu cantik`")
     sleep(2)
-    await typew.edit("`Kedua kamu manis`")
+    await sad.edit("`Kedua kamu manis`")
     sleep(1)
-    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+    await sad.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
 
 
 # Create by myself @localheart
-
-
-@register(outgoing=True, pattern="^.stres(?: |$)(.*)")
+@toni_cmd(pattern="lahk(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**fajar Peler☑️**")
-    await typew.edit("**fajar Peler✅**")
+    lah = await edit_or_reply(typew, "`Lahk, Lo tolol?`")
     sleep(1)
-    await typew.edit("**putra Gilaa☑️**")
-    await typew.edit("**putra Gilaa✅**")
-    sleep(2)
-    await typew.edit("**duta Depresi☑️**")
-    await typew.edit("**duta Depresi✅**")
-    sleep(2)
-    await typew.edit("**askar Gajelas☑️**")
-    await typew.edit("**askar Gajelas✅**")
-    sleep(2)
-    await typew.edit("**ultra-man GJM!☑️**")
-    await typew.edit("**ultra-man GJM!✅**")
-    sleep(2)
-    await typew.edit("**kek GJB!☑️**")
-    await typew.edit("**kek GJB!✅**")
-    sleep(3)
-    await typew.edit("**CUMA TONIC YANG BAIK HATI DAN TIDAK SOMBONG✨**")
+    await lah.edit("`Apa dongok?`")
+    sleep(1)
+    await lah.edit("`Gausah sok keras`")
+    sleep(1)
+    await lah.edit("`Gua ga ketrigger sama bocah baru nyemplung!`")
 
 
-@register(outgoing=True, pattern="^.lahk(?: |$)(.*)")
+@toni_cmd(pattern="wah(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Lahk, Lo tolol?`")
-    sleep(1)
-    await typew.edit("`Apa dongok?`")
-    sleep(1)
-    await typew.edit("`Gausah sok keras`")
-    sleep(1)
-    await typew.edit("`Gua ga ketrigger sama bocah baru nyemplung!`")
-
-
-@register(outgoing=True, pattern="^.wah(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("`Wahh, War nya keren bang`")
+    wah = await edit_or_reply(typew, "`Wahh, War nya keren bang`")
     sleep(2)
-    await typew.edit("`Tapi, Yang gua liat, kok Kaya lawakan`")
+    await wah.edit("`Tapi, Yang gua liat, kok Kaya lawakan`")
     sleep(2)
-    await typew.edit("`Oh iya, Kan lo badut 🤡`")
+    await wah.edit("`Oh iya, Kan lo badut 🤡`")
     sleep(2)
-    await typew.edit("`Kosa kata pas ngelawak, Jangan di pake war bang`")
+    await wah.edit("`Kosa kata pas ngelawak, Jangan di pake war bang`")
     sleep(2)
-    await typew.edit("`Kesannya lo ngasih kita hiburan.`")
+    await wah.edit("`Kesannya lo ngasih kita hiburan.`")
     sleep(2)
-    await typew.edit(
+    await wah.edit(
         "`Kasian badut🤡, Ga di hargain pengunjung, Eh lampiaskan nya ke Tele, Wkwkwk`"
     )
     sleep(3)
-    await typew.edit("`Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak`")
+    await wah.edit("`Dah sana cabut, Makasih hiburannya, Udah bikin Gua tawa ngakak`")
 
 
 CMD_HELP.update(
     {
-        "skyzubot": "`.stres`\
-    \nUsage: menampilkan alive bot.\
-    \n\n`.sadboy`\
-    \nUsage: hiks\
-    \nUsage: misi."
-    }
+    "tonicubot": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}sadboy`\
+    \n↳ : Biasalah sadboy hikss\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}wah`\
+    \n↳ : Ngatain orang war\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}lahk`\
+    \n↳ : Ngatain orang sok keras.\."
+}
 )
