7318308264509bcf8d009385683d8eb60a73b0f2
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-11 09:33:02 +0700

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -1,94 +1,198 @@
 from platform import uname
-
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
+from userbot import ALIVE_NAME, CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, toni_cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^D(?: |$)(.*)")
+@toni_cmd(pattern='d(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**BACOT NGENTOTTT BAPA LU SURUH RIBUT SAMA GUA**")
+    await edit_or_reply(typew, "**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
 
 
-@register(outgoing=True, pattern="^E(?: |$)(.*)")
+@toni_cmd(pattern='e(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
+    await edit_or_reply(typew, "**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
 
 
-@register(outgoing=True, pattern="^F(?: |$)(.*)")
+@toni_cmd(pattern='f(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
+    await edit_or_reply(typew, "**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern="^I(?: |$)(.*)")
+@toni_cmd(pattern='i(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
+    await edit_or_reply(typew, "**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern="^Q(?: |$)(.*)")
+@toni_cmd(pattern='r(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**EHH GOBLOK LU SEMUA RIBUT SAMA GUA SINI NGENTOT!**")
+    await edit_or_reply(typew, "**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern="^R(?: |$)(.*)")
+@toni_cmd(pattern='t(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
+    await edit_or_reply(typew, "**BABI!!KONTOL!!NGENTOT!!!**")
 
 
-@register(outgoing=True, pattern="^T(?: |$)(.*)")
+@toni_cmd(pattern='u(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**BABI!!KONTOL!!NGENTOT!!!**")
+    await edit_or_reply(typew, "**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern="^U(?: |$)(.*)")
+@toni_cmd(pattern='w(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA BHAHAHAHA**")
+    await edit_or_reply(typew, "**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern="^W(?: |$)(.*)")
+@toni_cmd(pattern='z(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
+    await edit_or_reply(typew, "**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern="^Q(?: |$)(.*)")
+@toni_cmd(pattern='k(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
+    await edit_or_reply(typew, "**HAI PERKENALKAN NAMA SAYA GAK TAU LUPA!!**")
+
+
+@toni_cmd(pattern='n(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
+
+
+@toni_cmd(pattern='b(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
+
+
+@toni_cmd(pattern='m(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
+
+
+@toni_cmd(pattern='c(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
+
+
+@toni_cmd(pattern='s(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**GAUSAH SOKAP DEH KAMU!!**")
+
+
+@toni_cmd(pattern='v(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**MACAM BAGUS AE LU BEGITU HMM!!**")
+
+
+@toni_cmd(pattern='j(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**MAAF BUKAN JAGOAN HAHAHAHA!!**")
+
+
+@toni_cmd(pattern='a(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**BISMILLAH SLEEP CALL!!😁**")
+
+
+@toni_cmd(pattern='g(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**GANTENG LU BEGITU???**")
+
+
+@toni_cmd(pattern='y(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
+
+
+@toni_cmd(pattern='h(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**CANTIK LU BEGITU???**")
+
+
+@toni_cmd(pattern='o(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
+
+
+@toni_cmd(pattern='1(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await edit_or_reply(typew, "**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
 
 
 CMD_HELP.update(
     {
-        "toxic": "D\
+    "toxic":
+    f"{cmd}d\
 \nUsage: Bacotin Orang.\
-\n\nE\
+\n\n{cmd}e\
 \nUsage: Buat Orang Yang Sok Keras.\
-\n\nF\
+\n\n{cmd}f\
 \nUsage: Ngatain Orang Wkwkkw.\
-\n\nI\
+\n\n{cmd}i\
 \nUsage: Kontol Orang Ngatain.\
-\n\nQ\
-\nUsage: Ngajak Ribut Orang.\
-\n\nR\
+\n\n{cmd}r\
 \nUsage: Pantun Anjing.\
-\n\nT\
+\n\n{cmd}t\
 \nUsage: Nyebutin Binatang.\
-\n\nU\
+\n\n{cmd}u\
 \nUsage: Biar Dikata Ganteng.\
-\n\nW\
+\n\n{cmd}w\
 \nUsage: Biar Dikata Cantik.\
-\n\nQ\
-\nUsage: Tremor Kan Lu."
-    }
+\n\n{cmd}z\
+\nUsage: Tremor Kan Lu.\
+\n\n{cmd}k\
+\nUsage: Memperkenalkan Diri.\
+\n\n{cmd}n\
+\nUsage: Menanyakan Kabar.\
+\n\n{cmd}b\
+\nUsage: Sok Kepinteran.\
+\n\n{cmd}m\
+\nUsage: Gc Nya Kaya kuburan.\
+\n\\{cmd}c\
+\nUsage: Dia tuh Ngeyel banget.\
+\n\n{cmd}s\
+\nUsage: Haha sokap.\
+\n\n{cmd}v\
+\nUsage: Merendah.\
+\n\n{cmd}a\
+\nUsage: Nyari Sleep Call.\
+\n\n{cmd}j\
+\nUsage: Hujat yang gapunya muka.\
+\n\n{cmd}g\
+\nUsage: Kegantengan.\
+\n\n{cmd}y\
+\nUsage: teruntuk petarung.\
+\n\n{cmd}h\
+\nUsage: Kecantikan.\
+\n\n{cmd}o\
+\nUsage: Ngatain org norak.\
+\n\n{cmd}1\
+\nUsage: Ngatain Petarunx Tele."
+}
 )
