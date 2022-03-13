19f8b2573fcab2396ec3740e8c7c756988a7064a
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-13 19:02:15 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -18,12 +18,14 @@ from telethon.tl.types import (
     DocumentAttributeFilename,
 )
 
+
 async def md5(fname: str) -> str:
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
 
 def media_type(message):
     if message and message.photo:
@@ -132,74 +134,6 @@ async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Op
     return thumb_image_path if os.path.exists(thumb_image_path) else None
 
 
-async def edit_or_reply(
-    event,
-    text,
-    parse_mode=None,
-    link_preview=None,
-    file_name=None,
-    aslink=False,
-    linktext=None,
-    caption=None,
-):
-    link_preview = link_preview or False
-    reply_to = await event.get_reply_message()
-    if len(text) < 4096:
-        parse_mode = parse_mode or "md"
-        if event.sender_id in SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(
-                    text, parse_mode=parse_mode, link_preview=link_preview
-                )
-            return await event.reply(
-                text, parse_mode=parse_mode, link_preview=link_preview
-            )
-        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
-        return event
-    asciich = ["*", "`", "_"]
-    for i in asciich:
-        text = re.sub(rf"\{i}", "", text)
-    if aslink:
-        linktext = linktext or "Pesan terlalu besar jadi ditempel ke nekobin"
-        try:
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": text}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            text = linktext + f" [Disini](https://nekobin.com/{key})"
-        except Exception:
-            text = re.sub(r"•", ">>", text)
-            kresult = requests.post(
-                "https://del.dog/documents", data=text.encode("UTF-8")
-            ).json()
-            text = linktext + f" [Disini](https://del.dog/{kresult['key']})"
-        if event.sender_id in SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(text, link_preview=link_preview)
-            return await event.reply(text, link_preview=link_preview)
-        await event.edit(text, link_preview=link_preview)
-        return event
-    file_name = file_name or "output.txt"
-    caption = caption or None
-    with open(file_name, "w+") as output:
-        output.write(text)
-    if reply_to:
-        await reply_to.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    if event.sender_id in SUDO_USERS:
-        await event.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    await event.client.send_file(event.chat_id, file_name, caption=caption)
-    await event.delete()
-    os.remove(file_name)
-
-
 async def check_media(reply_message):
     if reply_message and reply_message.media:
         if reply_message.photo:
@@ -240,6 +174,10 @@ async def run_cmd(cmd: list) -> tuple[bytes, bytes]:
     e_resp = err.strip()
     return t_resp, e_resp
 
+
+# https://github.com/TeamUltroid/pyUltroid/blob/31c271cf4d35ab700e5880e952e54c82046812c2/pyUltroid/functions/helper.py#L154
+
+
 async def bash(cmd):
     process = await asyncio.create_subprocess_shell(
         cmd,
@@ -266,11 +204,75 @@ def post_to_telegraph(title, html_format_content):
     return post_page["url"]
 
 
+async def reply_id(event):
+    reply_to_id = None
+    if event.sender_id in SUDO_USERS:
+        reply_to_id = event.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    return reply_to_id
+
+
+async def edit_or_reply(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    deflink=False,
+    noformat=False,
+    linktext=None,
+    caption=None,
+):
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096 and not deflink:
+        parse_mode = parse_mode or "md"
+        if not event.out and event.sender_id:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+        return event
+    if not noformat:
+        text = md_to_text(text)
+    if aslink or deflink:
+        linktext = linktext or "**Pesan Terlalu Panjang**"
+        response = await paste_message(text, pastetype="s")
+        text = linktext + f" [Lihat Disini]({response})"
+        if not event.out and event.sender_id:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        await event.edit(text, link_preview=link_preview)
+        return event
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if not event.out and event.sender_id:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
+
+
 async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
     parse_mode = parse_mode or "md"
     link_preview = link_preview or False
-    time = time or 5
-    if event.sender_id in SUDO_USERS:
+    time = time or 15
+    if not event.out and event.sender_id:
         reply_to = await event.get_reply_message()
         newevent = (
             await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
@@ -285,6 +287,7 @@ async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None
         )
     await asyncio.sleep(time)
     return await newevent.delete()
+
 
 eod = edit_delete
 
@@ -340,12 +343,3 @@ def download_lagu(url: str) -> str:
     info = ydl.extract_info(url, download=False)
     ydl.download([url])
     return os.path.join("downloads", f"{info['id']}.{info['ext']}")
-
-
-async def reply_id(event):
-    reply_to_id = None
-    if event.sender_id in SUDO_USERS:
-        reply_to_id = event.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    return reply_to_id
