f90b4940ee2f1d061eb8aad994221e3b9fc9c005
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-26 09:55:16 +0700

    Create logger.py

---
+++ userbot/clients/logger.py
@@ -0,0 +1,62 @@
+# Credit @mrismanaziz
+
+import asyncio
+
+from telethon.tl.functions.channels import EditAdminRequest, InviteToChannelRequest
+from telethon.tl.types import ChatAdminRights
+
+from userbot import (
+  BOT_VER as version,
+  BOTLOG_CHATID,
+  CMD_HANDLER as cmd,
+  branch,
+  tgbot,
+  bot,
+)
+from userbot.utils import checking
+
+MSG_ON = """
+✨ **Tonic-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}@{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+➠ **Powered By:** @PrimeSupportGroup
+━━
+"""
+async def toni_userbot_on():
+    new_rights = ChatAdminRights(
+        add_admins=True,
+        invite_users=True,
+        change_info=True,
+        ban_users=True,
+        delete_messages=True,
+        pin_messages=True,
+        manage_call=True,
+    )
+    try:
+        if bot and tgbot:
+            ToniUBOT = await tgbot.get_me()
+            BOT_USERNAME = ToniUBOT.username
+            await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+            await asyncio.sleep(3)
+    except BaseException:
+        pass
+    try:
+        if bot and tgbot:
+            ToniUBOT = await tgbot.get_me()
+            BOT_USERNAME = ToniUBOT.username
+            await bot(EditAdminRequest(BOTLOG_CHATID, BOT_USERNAME, new_rights, "BOT"))
+            await asyncio.sleep(3)
+    except BaseException:
+        pass
+    try:
+        if bot:
+            await checking(bot)
+            await asyncio.sleep(3)
+            if BOTLOG_CHATID != 0:
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
