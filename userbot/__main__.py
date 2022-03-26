a7f7cf23bd6913d0d81605b127ee676eea4f382c
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-26 10:48:59 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -43,11 +43,11 @@ except BaseException as e:
     sys.exit(1)
 
 
-LOOP.run_until_complete(toni_userbot_on())
+bot.loop.run_until_complete(toni_userbot_on())
 if not BOTLOG_CHATID:
-    LOOP.run_until_complete(autopilot())
+    bot.loop.run_until_complete(autopilot())
 if not BOT_TOKEN:
-    LOOP.run_until_complete(autobot())
+    bot.loop.run_until_complete(autobot())
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
