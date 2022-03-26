0577c7a50a98fed9cd7541f42387c87348e16007
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-26 09:58:44 +0700

    Create __init__.py

---
+++ userbot/clients/__init__.py
@@ -0,0 +1,9 @@
+# Man - UserBot
+# Copyright (c) 2022 Man-Userbot
+# Credits: @mrismanaziz || https://github.com/mrismanaziz
+#
+# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from .logger import toni_userbot_on
+from .startup import toni_client, multi_toni
