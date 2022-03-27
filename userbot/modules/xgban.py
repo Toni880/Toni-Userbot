bf3e6cfc2072cda857e286ee53e9ec11698b7c7c
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-03-27 09:55:31 +0700

    Update xgban.py

--- userbot/modules/xgban.py
+++ userbot/modules/xgban.py
@@ -1,47 +1,36 @@
 from asyncio import sleep
-
-from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
+from telethon.tl.functions.channels import EditBannedRequest
+from userbot.utils import edit_or_reply, edit_delete, toni_cmd
+from userbot import CMD_HELP, CMD_HANDLER as cmd
 
-from userbot import CMD_HELP
-from userbot.events import register
 
-
-# Port By @VckyouuBitch From GeezProject
-# Perkontolan Dengan Hapus Credits
-@register(outgoing=True, pattern="^.allban(?: |$)(.*)")
+@toni_cmd(pattern="allban(?: |$)(.*)")
 async def testing(event):
     nikal = await event.get_chat()
     chutiya = await event.client.get_me()
     admin = nikal.admin_rights
     creator = nikal.creator
     if not admin and not creator:
-        await event.edit("Anda Tidak Mempunyai Hak")
+        await edit_or_reply(event, "Anda Tidak Mempunyai Hak")
         return
-    await event.edit("Tidak Melakukan Apa-apa")
-    # Thank for Dark_Cobra
+    await edit_or_reply(event, "Tidak Melakukan Apa-apa")
+# Thank for Dark_Cobra
     everyone = await event.client.get_participants(event.chat_id)
     for user in everyone:
         if user.id == chutiya.id:
             pass
         try:
-            await event.client(
-                EditBannedRequest(
-                    event.chat_id,
-                    int(user.id),
-                    ChatBannedRights(until_date=None, view_messages=True),
-                )
-            )
+            await event.client(EditBannedRequest(event.chat_id, int(user.id), ChatBannedRights(until_date=None, view_messages=True)))
         except Exception as e:
-            await event.edit(str(e))
-        await sleep(0.5)
-    await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
-
+            await edit_or_reply(event, str(e))
+        await sleep(.5)
+    await edit_delete(event, "Tidak Ada yang Terjadi di sini🙃🙂")
 
 CMD_HELP.update(
     {
-        "allban": "**Plugin : **`allban`\
-    \n\n**Syntax : **`.allban`\
+        "allban": f"**Plugin : **`allban`\
+    \n\n**Syntax : **`{cmd}allban`\
     \n**Function : **ban all members in 1 cmnd"
     }
 )
