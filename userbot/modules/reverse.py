85e65ce8dcaf9f3cd2906200787317303f0a2c9b
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2022-04-01 18:20:46 +0700

    Update reverse.py

--- userbot/modules/reverse.py
+++ userbot/modules/reverse.py
@@ -16,15 +16,15 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
-from userbot import CMD_HELP, bot
-from userbot.events import register
+from userbot import CMD_HANDLER as cmd, CMD_HELP, bot
+from userbot.utils import toni_cmd
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
 
 
-@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
+@toni_cmd(pattern=r"reverse(?: |$)(\d*)")
 async def okgoogle(img):
     """For .reverse command, Google search images and stickers."""
     if os.path.isfile("okgoogle.png"):
@@ -144,7 +144,7 @@ async def scam(results, lim):
 
 CMD_HELP.update(
     {
-        "reverse": ">`.reverse`"
+        "reverse": f">`{cmd}reverse`"
         "\nUsage: Balas gambar/stiker untuk melakukan pencarian terbalik di google"
     }
 )
