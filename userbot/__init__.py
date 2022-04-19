8b3d1e8bc5003b0251838b60f5a40cb56b5bc002
ᴛᴏɴɪ - ᴇx <103916927+Toni880@users.noreply.github.com>
2022-04-19 12:26:31 +0700

    None

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -166,8 +166,8 @@ if G_BAN_LOGGER_GROUP:
 
 # Heroku Credentials for updater.
 HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 
 # JustWatch Country
 WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "ID")
